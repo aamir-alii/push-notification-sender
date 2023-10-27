e5590caf8330e9c8a56f34dda4c8b5358ebed3d2
Aamir Ali <aamir.ali@softcircles.org>
2023-10-27 14:31:50 +0500

    made changes

---
+++ index.js
@@ -0,0 +1,120 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const multer = require("multer");
+require("dotenv").config();
+const admin = require("firebase-admin");
+
+const multerStorage = multer.diskStorage({
+  filename: (req, file, cb) => {
+    cb(null, "server-config.json");
+  },
+  destination: (req, file, cb) => {
+    let folderPath = path.join(__dirname, "/public/server-config");
+    cb(null, folderPath);
+  },
+});
+
+const multerUpload = multer({
+  storage: multerStorage,
+  fileFilter: (req, file, cb) => {
+    const ext = file.mimetype.split("/")[1];
+    if (ext === "json") {
+      return cb(null, true);
+    }
+    cb("file must be of type json", false);
+  },
+});
+
+const upload = multerUpload.single("file");
+app.post("/send-token", upload, async (req, res) => {
+  try {
+    let tokens = req.body.tokens;
+    if (!tokens?.length) {
+      return res.status(400).json({
+        status: false,
+        messsage: "Please provide tokens",
+      });
+    }
+    if (!req.file) {
+      return res.status(400).json({
+        status: false,
+        messsage: "Please provide serviceAccountKey file",
+      });
+    }
+    let appName = `app-${Date.now()}`;
+    const serviceAccount = require(req.file.destination +
+      "/" +
+      req.file.filename);
+    let customApp = admin.initializeApp(
+      {
+        credential: admin.credential.cert(serviceAccount),
+      },
+      appName
+    );
+    for (let i = 0; i < tokens.length; i++) {
+      let token = tokens[i];
+      console.log(token);
+      if (!token?.length <= 0) continue;
+      try {
+        await customApp.messaging().send({
+          token,
+          data: {
+            roomId: "1" ?? "",
+            alarmId: "1" ?? "",
+            alarmSound: "sound1" ?? "",
+            severity: "1" ?? "",
+            roomData: "",
+          },
+          notification: {
+            title: "Dummy Title",
+            body: "Dummy body",
+          },
+          android: {
+            notification: {
+              channel_id: `360alerts${"sound1"}${1}`,
+            },
+          },
+          apns: {
+            payload: {
+              aps: {
+                sound: "smsmedium.mp3", // Replace with your custom sound file name (iOS)
+              },
+            },
+          },
+        });
+        console.log("Sent successfully");
+        console.log("Sent successfully");
+        console.log("Sent successfully");
+        console.log("Sent successfully");
+      } catch (error) {
+        console.log("Got error while sending notification");
+        console.log("Got error while sending notification");
+        return res.status(500).json({
+          status: false,
+          message: error.message,
+          error,
+        });
+        console.log(error);
+      }
+    }
+    customApp.delete();
+    res.status(200).json({
+      status: true,
+      message: "Notification send successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: false,
+      message: err.message,
+      error: err,
+    });
+  }
+});
+
+app.use("/", (req, res) => {
+  res.send("Api Running...");
+});
+app.listen(process.env.PORT, () => {
+  console.log("Server listening on port " + process.env.PORT);
+});
